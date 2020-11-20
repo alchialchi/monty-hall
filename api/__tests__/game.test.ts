c328106e023d5438171da3edc2d34013cde98a01
Lukina Olga <obeskurajen@mail.ru>
2020-11-20 13:11:27 +0100

    Remove unused file, rename to lowercase

--- api/__tests__/game.test.ts
+++ api/__tests__/game.test.ts
@@ -1,4 +1,6 @@
-import { playMontyHall } from "../Game";
+import { playMontyHall } from "../game";
+
+// playMontyHall(switchDoor: boolean, doorSelectedByMe: number, doorNumberWithCar: numder)
 
 test("Switch door: false, user selected door 1, the car is behind door 1", () => {
   expect(playMontyHall(false, 1, 1)).toBe(true);
